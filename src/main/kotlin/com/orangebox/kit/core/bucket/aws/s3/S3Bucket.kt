a3146201643244a3f0ab98500bbd6a5dc94bb66a
Diego Belli Mondego <diego@orangebox.technology>
2023-03-15 08:21:20 -0300

    fix s3 bucket process

--- src/main/kotlin/com/orangebox/kit/core/bucket/aws/s3/S3Bucket.kt
+++ src/main/kotlin/com/orangebox/kit/core/bucket/aws/s3/S3Bucket.kt
@@ -1,10 +1,12 @@
 package com.orangebox.kit.core.bucket.aws.s3
 
 import com.orangebox.kit.core.bucket.Bucket
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import software.amazon.awssdk.core.async.AsyncRequestBody
 import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import java.lang.IllegalArgumentException
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 
@@ -12,20 +14,35 @@ import javax.inject.Inject
 @ApplicationScoped
 class S3Bucket : Bucket() {
 
+    @ConfigProperty(name = "orangekit.core.bucket.name", defaultValue = "ERROR")
+    private lateinit var bucketName: String
+
+    @ConfigProperty(name = "quarkus.s3.aws.region")
+    private lateinit var region: String
+
     @Inject
     lateinit var s3Client: S3AsyncClient
 
     override fun saveFile(name: String, sufix: String?, data: ByteArray): String {
+
+        if(bucketName == "ERROR"){
+            throw IllegalArgumentException("orangekit.core.bucket.name must be provided in .env")
+        }
+
+        var fileName = name
+        if(sufix != null){
+            fileName = "$name/$sufix"
+        }
+
         val req = PutObjectRequest.builder()
-            .bucket(params["bucketName"])
-            .key(name)
+            .bucket(bucketName)
+            .key(fileName)
             .contentType(params["contentType"])
             .contentLength(data.size.toLong())
             .build()
 
         s3Client.putObject(req, AsyncRequestBody.fromBytes(data))
-
-        return "https://" + params["bucketName"] + ".s3-sa-east-1.amazonaws.com/" + name
+        return "https://$bucketName.s3-$region.amazonaws.com/$fileName"
     }
 
     override fun deleteFile(name: String) {
