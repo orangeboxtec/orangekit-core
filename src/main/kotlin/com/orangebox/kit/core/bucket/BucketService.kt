9dff5332725684162e655277c4887d59d0320bb1
Diego Mondego <diego@orangebox.technology>
2023-03-24 13:57:18 -0300

    fix s3 upload

--- src/main/kotlin/com/orangebox/kit/core/bucket/BucketService.kt
+++ src/main/kotlin/com/orangebox/kit/core/bucket/BucketService.kt
@@ -112,7 +112,7 @@ class BucketService {
         val data: ByteArray = if (fileUpload.fileBytes != null) {
             fileUpload.fileBytes!!
         } else {
-            fileUpload.file = fileUpload.file?.split(",")?.get(0)
+            fileUpload.file = fileUpload.file?.split(",")?.last()
             Base64.decodeBase64(fileUpload.file)
         }
         return bucket.saveFile(namePrefix, fileUpload.idObject, data)
