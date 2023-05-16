71c606ef46cfa7de500af7076bcf4c76272715de
Diego Mondego <diego@orangebox.technology>
2023-05-16 07:42:49 -0300

    new fields in GeneralUser

--- src/main/kotlin/com/orangebox/kit/core/user/GeneralUser.kt
+++ src/main/kotlin/com/orangebox/kit/core/user/GeneralUser.kt
@@ -3,7 +3,10 @@ package com.orangebox.kit.core.user
 interface GeneralUser {
     val id: String?
     val email: String?
+    var name: String?
+    var lastName: String?
     val tokenFirebase: String?
     val phoneCountryCode: Int?
     val phoneNumber: Long?
+    var urlImage: String?
 }
