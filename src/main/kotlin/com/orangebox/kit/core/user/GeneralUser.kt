e2946b38d71093470e01fe6715627113bc743661
Diego Mondego <diego@orangebox.technology>
2023-05-16 08:07:58 -0300

    UserCard

--- src/main/kotlin/com/orangebox/kit/core/user/GeneralUser.kt
+++ src/main/kotlin/com/orangebox/kit/core/user/GeneralUser.kt
@@ -1,9 +1,21 @@
 package com.orangebox.kit.core.user
 
-interface GeneralUser {
-    val id: String?
-    val email: String?
-    val tokenFirebase: String?
-    val phoneCountryCode: Int?
-    val phoneNumber: Long?
+class GeneralUser {
+    val id: String? = null
+    val email: String? = null
+    val tokenFirebase: String? = null
+    val phoneCountryCode: Int? = null
+    val phoneNumber: Long? = null
+    var name: String? = null
+    var lastName: String? = null
+    var urlImage: String? = null
+
+    fun toCard(): UserCard{
+        val card = UserCard()
+        card.id = this.id
+        card.name = this.name
+        card.lastName = this.lastName
+        card.urlImage = this.urlImage
+        return card
+    }
 }
