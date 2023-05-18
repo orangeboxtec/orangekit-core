c74481243065fe1ac2b064cee0db8db915aad178
Diego Mondego <diego@orangebox.technology>
2023-05-18 09:24:37 -0300

    App Token

--- src/main/kotlin/com/orangebox/kit/core/apptoken/AppTokenDAO.kt
+++ src/main/kotlin/com/orangebox/kit/core/apptoken/AppTokenDAO.kt
@@ -1,7 +1,9 @@
 package com.orangebox.kit.core.apptoken
 
 import com.orangebox.kit.core.dao.AbstractDAO
+import javax.enterprise.context.ApplicationScoped
 
+@ApplicationScoped
 class AppTokenDAO : AbstractDAO<AppToken>(AppToken::class.java) {
     override fun getId(obj: AppToken): Any {
         return obj.id!!
