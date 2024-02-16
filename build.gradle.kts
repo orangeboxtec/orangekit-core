249373109d8a9889aca029b88a3e6055ec251c21
Diego Mondego <diego@orangebox.technology>
2024-02-16 18:05:13 -0300

    ok 2.0 upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import io.quarkus.gradle.tasks.QuarkusBuild
 import org.kordamp.gradle.plugin.jandex.tasks.JandexTask
 
 plugins {
@@ -10,7 +9,7 @@ plugins {
 }
 
 group = "com.orangebox.kit.core"
-version = "2.0.0"
+version = "2.0.1"
 
 repositories {
     mavenCentral()
