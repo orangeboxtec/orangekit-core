58435194a6e7d4b08154651cdfaac91d39894743
Diego Mondego <diego@orangebox.technology>
2024-11-05 22:58:17 -0300

    idObj in AppToken.kt

--- src/main/kotlin/com/orangebox/kit/core/apptoken/AppToken.kt
+++ src/main/kotlin/com/orangebox/kit/core/apptoken/AppToken.kt
@@ -8,6 +8,7 @@ import java.util.*
 class AppToken {
     @OKId
     var id: String? = null
+    var idObj: String? = null
     var token: String? = null
     var name: String? = null
     var creationDate: Date? = null
